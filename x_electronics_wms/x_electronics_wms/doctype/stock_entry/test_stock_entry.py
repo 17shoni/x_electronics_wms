73288454d8e80fdd6bcccfc982cc41ee3c509678
Victor Musyoni Mutua <victormutua17@gmail.com>
2026-08-13 10:32:57 +0300

    feat: add Stock Entry transaction DocTypes

---
+++ x_electronics_wms/x_electronics_wms/doctype/stock_entry/test_stock_entry.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Victor Musyoni Mutua and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestStockEntry(FrappeTestCase):
+	pass
